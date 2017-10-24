20a77c3e47ccaba95f047b586d9e7997fb540a98
James Galante <jamesgalante@Jamess-MacBook-Pro.local>
2017-10-24 17:12:38 -0400

    updated controller and objective class

--- src/PopController.java
+++ src/PopController.java
@@ -1,4 +1,66 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class PopController {
+	public static final int EASY = 0;
+	public static final int MEDIUM = 1;
+	public static final int HARD = 1;
 
+	
+	public int difficulty; //determines speed of game
+
+	static Collection<String> Objectives = new ArrayList<String>(); 
+	public String objective1 = "no objective set";
+	public String objective2 = "no objective set";
+	public String objective3 = "no objective set";
+
+
+	public PopController(int diff) {
+		this.difficulty = diff;
+	}
+	
+	public static void main(String[] args) {
+		PopController Game = new PopController(EASY);
+		Game.setObjectives();
+
+		
+	}
+	
+	//picks 3 random objectives from "Objectives" class
+	public void setObjectives() {
+		Objective obj = new Objective();
+	
+		objective1 = obj.getObjective();
+		objective2 = obj.getObjective();
+		
+		//make sure objective 2 != objective 2
+		while (objective2 == objective1) {
+			objective2 = obj.getObjective();
+		}
+		
+		objective3 = obj.getObjective();
+		
+		//make sure objective 3 != objective 2/3
+		while (objective3 == objective1 || objective3 == objective2) { 
+			objective3 = obj.getObjective();
+		}
+		
+		Objectives.add(objective1);
+		Objectives.add(objective2);
+		Objectives.add(objective3);
+	}
+	
+	
+	public void setGrid() {
+		
+	}
+	
+	public void setGunList() {
+		
+	}
+	
+	
 }
+
+
