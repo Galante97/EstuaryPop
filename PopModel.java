1e89213b9a4f3f4a0db2df1204c332fef62baad2
Olivia Liepa <olivialiepa@Olivias-MacBook-Pro.local>
2017-10-30 02:44:12 -0400

    added setGrid and all shoot fns

--- PopModel.java
+++ PopModel.java
@@ -11,30 +11,40 @@ public class PopModel {
 	ArrayList<String> objectives = new ArrayList<String>();
 	Bubble[][] grid;
 	Bubble[] gunList;
-	//gunPosition
-	//time
 	int score;
 	boolean obj0done = false;
 	boolean obj1done = false;
 	boolean obj2done = false;
-	int bubbleImageWidth;
-	int bubbleImageHeight;
-	int gunImageWidth;
-	int gunImageHeight;
+	int bubbleImageWidth; // need a getter for this
+	int bubbleImageHeight; // need a getter for this
+	int gunImageWidth; // need a getter for this
+	int gunImageHeight; // need a getter for this
 	double gunXCoord;
 	double gunYCoord;
 	Boolean gunDirec = true;
-	double gunEdgeX = 0; //middle - gunImglen;
+	double gunEdgeX = 0; 
 	double gunEdgeY = 0;
-	int gridRows;
-	int gridColumns;
+	int gridRows; // must be initialized here
+	int gridColumns; // must be initialized here
+	int startGridRows; // number of rows we are starting with, must be initialized here
 	int degree;
 	int bubbleSpeed;
 	boolean clicked = false;
-	
+	Objective o1;
+	Objective o2;
+	Objective o3;
+	int gunBubblesX;  //determines spacing of gun bubbles
+	int gunBubblesY; //constant
+	int gridBubblesX = bubbleImageWidth/2; //determines xCoords grid bubbles
+	int gridBubblesY = bubbleImageHeight/2;
+	int contactX;
+	int contactY;
+
+	//set from controller
 	public void setClicked(boolean click) {
 		this.clicked = click;
 	}
+	
 	
 	public void checkMatch(){ // not complete yet
 		for(Bubble[] bArr: grid){
@@ -69,24 +79,76 @@ public class PopModel {
 		}
 	}
 	 
-	 
-	
 	public void chooseObjectives(){
-		//randomly selects 3 objectives from pool (where is the pool???)
+		o1.setObjective();
+		o2.setObjective();
+		o3.setObjective();
 	}
 	
 	public void setGrid(){
-		//fills the grid with bubbles
+		Objective obj = new Objective(); 
+		for(int i = 0; i<gridRows; i++) {
+			for (int j = 0; j<gridColumns; j++) {
+				while(i<startGridRows) { //fill starting rows with Bubbles with Objectives
+					obj.setObjective();
+					grid[i][j] = new Bubble(gridBubblesX + (i*gridBubblesX), gridBubblesY + (j*gridBubblesY), false, obj);
+				} //fill the rest with null objectives
+				grid[i][j] = new Bubble(gridBubblesX + (i*gridBubblesX), gridBubblesY + (j*gridBubblesY), false, null);
+			}
+		}
+			
 	}
 	
+	//checks to see where in the grid the bubble landed compared to the one it made contact with
+	//helper for shoot
+	public void addToGrid(Bubble b) { 
+		b.showGunImage = false;
+		if((contactX == b.xCoord)&&(contactY > b.yCoord)){//above contact bubble
+			grid[contactX][contactY-1] = b;
+		}
+		if((contactX > b.xCoord)&&(contactY > b.yCoord)){//above contact bubble, to the left (not needed for alpha, but needed for staggered shape)
+			grid[contactX-1][contactY-1] = b;
+		}
+		if((contactX < b.xCoord)&&(contactY > b.yCoord)){//above contact bubble, to the right (not needed for alpha, but needed for staggered shape)
+			grid[contactX+1][contactY-1] = b;
+		}
+		if((contactX == b.xCoord)&&(contactY < b.yCoord)){//below contact bubble
+			grid[contactX][contactY+1] = b;
+		}
+		if((contactX > b.xCoord)&&(contactY < b.yCoord)){//below contact bubble, to the left (not needed for alpha, but needed for staggered shape)
+			grid[contactX-1][contactY+1] = b;
+		}
+		if((contactX < b.xCoord)&&(contactY < b.yCoord)){//below contact bubble, to the right (not needed for alpha, but needed for staggered shape)
+			grid[contactX+1][contactY+1] = b;
+		}
+		if((contactX > b.xCoord)&&(contactY == b.yCoord)){//left of contact bubble
+			grid[contactX-1][contactY] = b;
+		}
+		if((contactX < b.xCoord)&&(contactY == b.yCoord)){//right of contact bubble
+			grid[contactX+1][contactY] = b;
+		}
+	}
+	
+	//only used at the beginning
+	//adds bubbles to the gun list until it is full
 	public void loadGun(){
-		//adds bubbles to the gun list until it is full
-		//used both for initialization and reloading the gun every time you shoot
+		Objective obj = new Objective();
+		for(int i = 0; i < gunList.length; i++) {
+			obj.setObjective(); 
+			gunList[i] = new Bubble(gunBubblesX + (i*gunBubblesX), gunBubblesY, true, obj);
+		}
 	}
 	
+	//reloads when we shoot
+	public void reloadGun() {
+		for(int i=0; i<gunList.length-1; i++) {
+			gunList[i] = gunList[i+1];
+		}
+		gunList[gunList.length-1] = new Bubble(gunList[gunList.length-2].xCoord + gunBubblesX, gunBubblesY, true, new Objective());
+	}
 	
 	//oscillation of gun that applies clicked as a mouse listener
-	
+	//ran throughout game
 	public void moveGun(){
 		int r = gunImageHeight; // radius - it will be the gun image length
 		while (clicked == false) {
@@ -107,27 +169,32 @@ public class PopModel {
 				gunDirec = false; //false means it is moving left
 			}
 		}
-		shoot(); // if clicked, gun pauses and shoots
+		shoot(degree, gunXCoord, gunYCoord); // if clicked, gun pauses and shoots
 	}
 			
 	
 	//ran during every frame of shoot - checks if it comes into contact with another bubble - if it does, we return true
-	public boolean stopBubble(Bubble b) {
-		if ((b.xCoord % (bubbleImageWidth/2) == 0) && (b.yCoord % (bubbleImageHeight/2) == 0)){ //sees if the xCoord and yCoord would place it into a 
+	//helper for shoot
+	public boolean stopBubble(Bubble b, int degr) {
+		if ((b.xCoord % (gridBubblesX) == 0) && (b.yCoord % (gridBubblesY) == 0)){ //sees if the xCoord and yCoord would place it into a 
 			//bubble spot so we don't have to check every single frame
-			if (degree < 45) { //check left half of grid - minimizes amount of bubbles to check
+			if (degr < 45) { //check left half of grid - minimizes amount of bubbles to check
 				for (int i = 0; i < gridColumns; i+=bubbleImageWidth) {
 					for (int j = 0; j < gridRows/2; j+=bubbleImageHeight) {
-						if (checkContact(b, grid[i][j])) { //or y = image height
+						if (checkContact(b, grid[i][j])) { //need to add or y = frame height
+							contactX = i;
+							contactY = j;
 							return true;
 						}
 					}
 				}
 			}
-			else if (degree >= 45) { // check right half of grid 
+			else if (degr >= 45) { // check right half of grid 
 				for (int i = 0; i < gridColumns; i+=bubbleImageWidth) {
 					for (int j = gridRows/2; j < gridRows; j+=bubbleImageHeight) {//call update helper
 						if (checkContact(b, grid[i][j])){
+							contactX = i;
+							contactY = j;
 							return true;
 						}
 					}
@@ -139,21 +206,24 @@ public class PopModel {
 
 	
 	//moves forward on an angle
-	public void moveBubbleForward(Bubble b) {
-		Long x = Math.round(bubbleSpeed * Math.sin(degree));
-		Long y = Math.round(bubbleSpeed * Math.cos(degree));
+	//helper for shoot
+	public void moveBubbleForward(Bubble b, int degr) {
+		Long x = Math.round(bubbleSpeed * Math.sin(degr));
+		Long y = Math.round(bubbleSpeed * Math.cos(degr));
 	
-		b.xCoord = x.intValue();
-		b.yCoord = y.intValue();
+		b.xCoord += x.intValue();
+		b.yCoord += y.intValue();
 		
 	}
 	
 	
 	//puts the gunBubble into the xCoord and yCoord of where the gun is and shoots it by moving the bubble until it comes in
 	//contact with another bubble
-	public void shoot(){
-		Long x = Math.round(gunXCoord);
-		Long y = Math.round(gunYCoord);
+	//takes degree, xCoordinate, and yCoordinate of the gun at the time when shoot is activated
+	//called by moveGun
+	public void shoot(int degr, double xCo, double yCo){
+		Long x = Math.round(xCo);
+		Long y = Math.round(yCo);
 	
 		
 		gunList[0].xCoord = x.intValue();
@@ -162,11 +232,16 @@ public class PopModel {
 		clicked = false; //start moving the gun again
 		moveGun();
 		
-		while (stopBubble(gunList[0]) == false) { //move the bubble forward until it makes contact
-			moveBubbleForward(gunList[0]);
+		while (stopBubble(gunList[0], degr) == false) { //move the bubble forward until it makes contact
+			moveBubbleForward(gunList[0], degr);
 		}
+		
+		addToGrid(gunList[0]);
+		
 		checkMatch(); //check if there are any matches that need to be popped
 		
+		gunList[0] = null;
+		reloadGun(); //removes old gunBubble and reloads gun
 		
 	}
 
